0f427ab3cc138efffc05cadb8aa70e8bb3beddc6
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-02-27 13:51:34 +0100

    Atualiza script de render do video para video handler e exporta

--- video-handler.js
+++ video-handler.js
@@ -1,51 +1,60 @@
 const spawn = require('child_process').spawn;
 const timecut = require('timecut');
 
-const recordConfig = {
-  url: 'http://localhost:5000',
-  viewport: {
-    width: 1280,
-    height: 720
-  },
-  selector: 'body',
-  fps: 30,
-  duration: 2.2,
-  output: 'video.mp4',
-  quiet: true,
-  inputAudio: 'piloto.mp3',
-  finalOutput: 'final.mp4',
-  convert: false,
+const buildVideo = async (config) => {
+  const defaultConfig = {
+    url: 'http://localhost:5000',
+    viewport: {
+      width: 1280,
+      height: 720
+    },
+    selector: 'body',
+    fps: 3,
+    duration: 2.2,
+    output: 'video.mp4',
+    quiet: true,
+    inputAudio: 'piloto.mp3',
+    finalOutput: 'final.mp4',
+    convert: false,
+  }
+  
+  console.log('Starting recording...')
+  const recordConfig = {...defaultConfig, ...config}
+  const loop = await timecut(recordConfig)
+  console.log('Recording finished')
+
+  if(!recordConfig.convert) {
+    console.log('Done');
+    return recordConfig.output;
+  }
+
+  const finalVideo = await loopVideoToMp3(recordConfig)
+  console.log('Done');
+  return recordConfig.finalOutput
 }
 
-console.log('Starting recording...')
-timecut(recordConfig)
-  .then(() => {
-    console.log('Recording finished')
-    if(!recordConfig.convert) {
-      return;
-    }
-
+const loopVideoToMp3 = async (config) => {
     const ffmpegArgs = [
       '-stream_loop',
       '-1',
       '-i',
       'video.mp4',
       '-i',
-      `assets/episodios/${recordConfig.input}`,
+      config.input,
       '-shortest',
       '-map',
       '0:v:0',
       '-map',
       '1:a:0',
       '-y',
-      recordConfig.finalOutput,
+      config.finalOutput,
     ];
 
     console.log('Starting video loop for audio duration...')
     const convertProcess = spawn('ffmpeg', ffmpegArgs);
     convertProcess.stderr.setEncoding('utf8');
     convertProcess.stderr.on('data', (data) => {
-      if(!recordConfig.quiet) {
+      if(!config.quiet) {
         console.log(data);
       }
     });
@@ -63,7 +72,6 @@ timecut(recordConfig)
         reject(err);
       });
     });
-  })
-  .then(() => {
-    console.log('Done!');
-  });
+  }
+
+  module.exports = buildVideo
